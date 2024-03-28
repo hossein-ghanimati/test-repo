3bcc0813e589d37eda2b038164a34600c6dbcfcf
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-03-28 17:35:17 +0330

    Add authFeacher Branch In Main Branch

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+const register = (userData) => {
+    if (!userData.length) return  false;
+
+    console.log(`The New User Register By That Data => ${userData}`);
+}
+
+const getMe = (userToken) => {
+    if (!userTokenlength) return  false;
+
+    console.log('Your Data IS => ', userToken);
+}
+const login = (userData) => {
+  if (!userData.password) return false
+
+  console.log(`Your Login Was SuccessFully.`)
+}
