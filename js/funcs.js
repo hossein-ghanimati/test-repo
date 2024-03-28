e3f34ce35cbfaccad13604e60504a96d14c9b09c
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-03-28 16:54:28 +0330

    Refactor getMe Function

--- js/funcs.js
+++ js/funcs.js
@@ -4,11 +4,9 @@ const login = () => {
 const register = (userData) => {
     console.log(`The New User Registered By Tht Data => ${userData}`)
 }
-const getMe (userToken) => {
-    if (userToken) {
-        return true
-    }else{
-        return false
-    }
+const getMe = (userToken) => {
+    if (!userToken) return false
+        
+    console.log(`User Token Was Valid, Token => ${userToken}`);
 }
     
