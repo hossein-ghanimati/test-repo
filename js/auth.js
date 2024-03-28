e8d87fc43842ec7f7bb5ac1fe4e0c97616565e27
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-03-28 17:27:18 +0330

    Add register And getMe Funcs In auth.js

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+const register = (userData) => {
+    if (!userData.length) return  false;
+
+    console.log(`The New User Register By That Data => ${userData}`);
+}
+
+const getMe = (userToken) => {
+    if (!userTokenlength) return  false;
+
+    console.log('Your Data IS => ', userToken);
+}
